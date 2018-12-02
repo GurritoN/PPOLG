c570e1c4f23d2f7be2d6238e383010c68e3c5bf5
GurritoN <gurritoglebon@gmail.com>
2018-12-02 20:08:37 +0300

    Added navigation drawer

---
+++ app/src/main/java/lab/gurriton/ppolg/ImeiActivity.kt
@@ -0,0 +1,58 @@
+package lab.gurriton.ppolg
+
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.appcompat.app.AlertDialog
+import android.telephony.TelephonyManager
+import android.widget.TextView
+import androidx.navigation.Navigation
+
+class ImeiActivity : AppCompatActivity() {
+
+    val READ_PHONE_STATE_REQUEST_CODE = 0
+
+    fun requestIMEI(){
+        val manager: TelephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+        val imei: TextView = findViewById(R.id.imei)
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
+            imei.text = manager.deviceId
+        }
+        else {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle("Permissions")
+                    .setMessage("This page need permissions to show IMEI")
+                    .setCancelable(false)
+                    .setNegativeButton("Got it!") { dialog, _ -> dialog.cancel() }
+                    .setOnCancelListener { ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_PHONE_STATE), READ_PHONE_STATE_REQUEST_CODE) }
+            val alert = builder.create()
+            alert.show()
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_imei)
+
+        requestIMEI()
+        val version: TextView = findViewById(R.id.version)
+        version.text = BuildConfig.VERSION_NAME
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode ) {
+            READ_PHONE_STATE_REQUEST_CODE -> {
+                val manager: TelephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+                val imei: TextView = findViewById(R.id.imei)
+                if (grantResults.isNotEmpty() and (grantResults[0] == PackageManager.PERMISSION_GRANTED))
+                    imei.text = manager.deviceId
+                else
+                    imei.text = "Unknown"
+            }
+        }
+    }
+}
--- app/src/main/java/lab/gurriton/ppolg/MainActivity.kt
+++ app/src/main/java/lab/gurriton/ppolg/MainActivity.kt
@@ -1,58 +1,83 @@
 package lab.gurriton.ppolg
 
-import android.Manifest
-import android.content.DialogInterface
-import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.app.ActivityCompat
-import androidx.appcompat.app.AlertDialog
-import android.telephony.TelephonyManager
-import android.widget.TextView
+import com.google.android.material.navigation.NavigationView
+import androidx.core.view.GravityCompat
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.Navigation
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    val READ_PHONE_STATE_REQUEST_CODE = 0
-
-    fun requestIMEI(){
-        val manager: TelephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
-        val imei: TextView = findViewById(R.id.imei)
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE) == PackageManager.PERMISSION_GRANTED) {
-            imei.text = manager.deviceId
-        }
-        else {
-            val builder = AlertDialog.Builder(this)
-            builder.setTitle("Permissions")
-                    .setMessage("This page need permissions to show IMEI")
-                    .setCancelable(false)
-                    .setNegativeButton("Got it!") { dialog, _ -> dialog.cancel() }
-                    .setOnCancelListener { ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.READ_PHONE_STATE), READ_PHONE_STATE_REQUEST_CODE) }
-            val alert = builder.create()
-            alert.show()
-        }
-    }
+    lateinit var drawer_layout : DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+        val toolbar : Toolbar = findViewById(R.id.toolbar)
+        drawer_layout = findViewById(R.id.drawer_layout)
+        val nav_view : NavigationView = findViewById(R.id.nav_view)
+        setSupportActionBar(toolbar)
 
-        requestIMEI()
-        val version: TextView = findViewById(R.id.version)
-        version.text = BuildConfig.VERSION_NAME
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode ) {
-            READ_PHONE_STATE_REQUEST_CODE -> {
-                val manager: TelephonyManager = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
-                val imei: TextView = findViewById(R.id.imei)
-                if (grantResults.isNotEmpty() and (grantResults[0] == PackageManager.PERMISSION_GRANTED))
-                    imei.text = manager.deviceId
-                else
-                    imei.text = "Unknown"
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_about -> {
+                Navigation.findNavController(this, R.id.nav_host).navigate(R.id.action_startFragment_to_imeiActivity2)
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_gallery -> {
+
+            }
+            R.id.nav_slideshow -> {
+
+            }
+            R.id.nav_manage -> {
+
+            }
+            R.id.nav_share -> {
+
+            }
+            R.id.nav_send -> {
+
             }
         }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
     }
 }
